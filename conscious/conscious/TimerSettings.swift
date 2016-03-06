c061b47ad19653109881153012482a8f97e0c521
Paul Thormahlen <pauldavid@gmail.com>
2016-03-06 12:49:44 -0800

    Adding basic model classes

---
+++ conscious/conscious/TimerSettings.swift
@@ -0,0 +1,13 @@
+//
+//  TimerSettings.swift
+//  conscious
+//
+//  Created by Paul Thormahlen on 3/6/16.
+//  Copyright © 2016 Conscious World. All rights reserved.
+//
+
+import Foundation
+
+class TimerSettings{
+
+}
